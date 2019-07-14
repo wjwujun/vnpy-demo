c47a2918c227fc9d19166a45233e7f1ca3eda122
wujun <1477563131@qq.com>
2019-07-14 22:04:33 +0800

    添加持仓和账户数据类

--- vnpy/trader/database/database_sql.py
+++ vnpy/trader/database/database_sql.py
@@ -64,6 +64,7 @@ class ModelBase(Model):
 
 
 def init_models(db: Database, driver: Driver):
+    #bar数据类
     class DbBarData(ModelBase):
         """
             定义k线表结构和字段
@@ -152,8 +153,177 @@ def init_models(db: Database, driver: Driver):
                     for c in chunked(dicts, 50):
                         DbBarData.insert_many(
                             c).on_conflict_replace().execute()
-
+    #tick数据类
     class DbTickData(ModelBase):
+        """
+            定义tick数据结构表字段
+        """
+
+        id = AutoField()
+
+        symbol: str = CharField()
+        exchange: str = CharField()
+        datetime: datetime = DateTimeField()
+
+        name: str = CharField()
+        volume: float = FloatField()
+        open_interest: float = FloatField()
+        last_price: float = FloatField()
+        last_volume: float = FloatField()
+        limit_up: float = FloatField()
+        limit_down: float = FloatField()
+
+        open_price: float = FloatField()
+        high_price: float = FloatField()
+        low_price: float = FloatField()
+        pre_close: float = FloatField()
+
+        bid_price_1: float = FloatField()
+        bid_price_2: float = FloatField(null=True)
+        bid_price_3: float = FloatField(null=True)
+        bid_price_4: float = FloatField(null=True)
+        bid_price_5: float = FloatField(null=True)
+
+        ask_price_1: float = FloatField()
+        ask_price_2: float = FloatField(null=True)
+        ask_price_3: float = FloatField(null=True)
+        ask_price_4: float = FloatField(null=True)
+        ask_price_5: float = FloatField(null=True)
+
+        bid_volume_1: float = FloatField()
+        bid_volume_2: float = FloatField(null=True)
+        bid_volume_3: float = FloatField(null=True)
+        bid_volume_4: float = FloatField(null=True)
+        bid_volume_5: float = FloatField(null=True)
+
+        ask_volume_1: float = FloatField()
+        ask_volume_2: float = FloatField(null=True)
+        ask_volume_3: float = FloatField(null=True)
+        ask_volume_4: float = FloatField(null=True)
+        ask_volume_5: float = FloatField(null=True)
+
+        class Meta:
+            database = db
+            indexes = ((("symbol", "exchange", "datetime"), True),)
+
+        @staticmethod
+        def from_tick(tick: TickData):
+            """
+            Generate DbTickData object from TickData.
+            """
+            db_tick = DbTickData()
+
+            db_tick.symbol = tick.symbol
+            db_tick.exchange = tick.exchange.value
+            db_tick.datetime = tick.datetime
+            db_tick.name = tick.name
+            db_tick.volume = tick.volume
+            db_tick.open_interest = tick.open_interest
+            db_tick.last_price = tick.last_price
+            db_tick.last_volume = tick.last_volume
+            db_tick.limit_up = tick.limit_up
+            db_tick.limit_down = tick.limit_down
+            db_tick.open_price = tick.open_price
+            db_tick.high_price = tick.high_price
+            db_tick.low_price = tick.low_price
+            db_tick.pre_close = tick.pre_close
+
+            db_tick.bid_price_1 = tick.bid_price_1
+            db_tick.ask_price_1 = tick.ask_price_1
+            db_tick.bid_volume_1 = tick.bid_volume_1
+            db_tick.ask_volume_1 = tick.ask_volume_1
+
+            if tick.bid_price_2:
+                db_tick.bid_price_2 = tick.bid_price_2
+                db_tick.bid_price_3 = tick.bid_price_3
+                db_tick.bid_price_4 = tick.bid_price_4
+                db_tick.bid_price_5 = tick.bid_price_5
+
+                db_tick.ask_price_2 = tick.ask_price_2
+                db_tick.ask_price_3 = tick.ask_price_3
+                db_tick.ask_price_4 = tick.ask_price_4
+                db_tick.ask_price_5 = tick.ask_price_5
+
+                db_tick.bid_volume_2 = tick.bid_volume_2
+                db_tick.bid_volume_3 = tick.bid_volume_3
+                db_tick.bid_volume_4 = tick.bid_volume_4
+                db_tick.bid_volume_5 = tick.bid_volume_5
+
+                db_tick.ask_volume_2 = tick.ask_volume_2
+                db_tick.ask_volume_3 = tick.ask_volume_3
+                db_tick.ask_volume_4 = tick.ask_volume_4
+                db_tick.ask_volume_5 = tick.ask_volume_5
+
+            return db_tick
+
+        def to_tick(self):
+            """
+            Generate TickData object from DbTickData.
+            """
+            tick = TickData(
+                symbol=self.symbol,
+                exchange=Exchange(self.exchange),
+                datetime=self.datetime,
+                name=self.name,
+                volume=self.volume,
+                open_interest=self.open_interest,
+                last_price=self.last_price,
+                last_volume=self.last_volume,
+                limit_up=self.limit_up,
+                limit_down=self.limit_down,
+                open_price=self.open_price,
+                high_price=self.high_price,
+                low_price=self.low_price,
+                pre_close=self.pre_close,
+                bid_price_1=self.bid_price_1,
+                ask_price_1=self.ask_price_1,
+                bid_volume_1=self.bid_volume_1,
+                ask_volume_1=self.ask_volume_1,
+                gateway_name="DB",
+            )
+
+            if self.bid_price_2:
+                tick.bid_price_2 = self.bid_price_2
+                tick.bid_price_3 = self.bid_price_3
+                tick.bid_price_4 = self.bid_price_4
+                tick.bid_price_5 = self.bid_price_5
+
+                tick.ask_price_2 = self.ask_price_2
+                tick.ask_price_3 = self.ask_price_3
+                tick.ask_price_4 = self.ask_price_4
+                tick.ask_price_5 = self.ask_price_5
+
+                tick.bid_volume_2 = self.bid_volume_2
+                tick.bid_volume_3 = self.bid_volume_3
+                tick.bid_volume_4 = self.bid_volume_4
+                tick.bid_volume_5 = self.bid_volume_5
+
+                tick.ask_volume_2 = self.ask_volume_2
+                tick.ask_volume_3 = self.ask_volume_3
+                tick.ask_volume_4 = self.ask_volume_4
+                tick.ask_volume_5 = self.ask_volume_5
+
+            return tick
+
+        @staticmethod
+        def save_all(objs: List["DbTickData"]):
+            dicts = [i.to_dict() for i in objs]
+            with db.atomic():
+                if driver is Driver.POSTGRESQL:
+                    for tick in dicts:
+                        DbTickData.insert(tick).on_conflict(
+                            update=tick,
+                            conflict_target=(
+                                DbTickData.symbol,
+                                DbTickData.exchange,
+                                DbTickData.datetime,
+                            ),
+                        ).execute()
+                else:
+                    for c in chunked(dicts, 50):
+                        DbTickData.insert_many(c).on_conflict_replace().execute()
+    #账户数据类
+    class DbAccountData(ModelBase):
         """
             定义tick数据结构表字段
         """
